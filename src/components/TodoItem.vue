6df3c4b4de78381a17b33e51b83df6be410196f9
encounter77 <749985146@qq.com>
2018-09-27 18:36:10 +0800

    Initial commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li @click="handleClick" class="item">
+    {{content}}
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],
+  methods: {
+    handleClick () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .item {
+        color: red;
+    }
+</style>
